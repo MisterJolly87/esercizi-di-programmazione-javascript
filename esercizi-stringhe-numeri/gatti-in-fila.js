418931332310e293ce6a4b51759aaaf7c885aabf
MisterJolly87 <63799097+MisterJolly87@users.noreply.github.com>
2020-04-19 16:12:27 +0200

    Update gatti-in-fila.js

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -19,3 +19,9 @@
 */
 
 var cats = 44;
+var inLine = 6;
+
+var numLines = Math.ceil(cats/inLine);
+var missing = numLines*inLine-cats;
+
+console.log(`$(cats) gatti in fila per $(inLine) si unirono compatti in $(numLines) file. Nell'ultima fila rimasero $(missing) posti gatto dispinibili.`);
